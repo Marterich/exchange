e956f0af7cadee8b037dd8e9dfa213bdbba305a9
Marterich <47688561+Marterich@users.noreply.github.com>
2021-08-31 12:49:40 +0200

    Add files via upload

--- ProxyShell-Check.ps1
+++ ProxyShell-Check.ps1
@@ -4,10 +4,25 @@ cls
 $wwwrootfolder = "C:\inetpub\wwwroot\"
  
 Write-Host "===== Check $wwwrootfolder =====" -ForegroundColor Black -BackgroundColor Yellow
-Get-ChildItem $wwwrootfolder -Recurse
+$files = Get-ChildItem $wwwrootfolder -Recurse
+foreach ($file in $files){
+if ($file.LastWriteTime -gt (date(08.2021))){
+Write-Host $file.Mode $file.LastWriteTime $file.Length $file.Name -ForegroundColor White -BackgroundColor Red
+}
+else {
+Write-Host $file.Mode $file.LastWriteTime $file.Length $file.Name}
+}
+
 
 Write-Host "===== Check $env:ExchangeInstallPath\FrontEnd\HttpProxy\owa =====" -ForegroundColor Black -BackgroundColor Yellow
-Get-ChildItem $env:ExchangeInstallPath\FrontEnd\HttpProxy\owa -Recurse
+$files = Get-ChildItem $env:ExchangeInstallPath\FrontEnd\HttpProxy\owa -Recurse
+foreach ($file in $files){
+if ($file.LastWriteTime -gt (date(08.2021))){
+Write-Host $file.Mode $file.LastWriteTime $file.Length $file.Name -ForegroundColor White -BackgroundColor Red
+}
+else{
+Write-Host $file.Mode $file.LastWriteTime $file.Length $file.Name}
+}
 
 Write-Host "===== Check Virtual Directory in Config Files  =====" -ForegroundColor Black -BackgroundColor Yellow
 Write-Host "File: C:\Windows\System32\inetsrv\Config\applicationHost.config" -ForegroundColor Black -BackgroundColor White
@@ -19,8 +34,13 @@ Write-Host "==== Check All Users =====" -ForegroundColor Black -BackgroundColor 
 Get-ChildItem "C:\Users\All Users"
 
 Write-Host "===== Check Exchange PST Export =====" -ForegroundColor Black -BackgroundColor Yellow
+$UserCredential = Get-Credential
+$exchange = [System.Net.DNS]::GetHostByName(($env:COMPUTERNAME)).HOstname
+$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$exchange/powershell -Authentication Kerberos -Credential $UserCredential
+Import-PSSession $session -DisableNameChecking -AllowClobber
 $MER = Get-MailboxExportRequest
 if ($MER -eq $null){Write-Host No PSTs Exported}
+Write-Host $MER -BackgroundColor Red -ForegroundColor White
 
 
 Write-Host "===== Check new AD Users last 30 days =====" -ForegroundColor Black -BackgroundColor Yellow
@@ -31,6 +51,7 @@ Get-ADUser -Filter * -Property whenCreated | Where {$_.whenCreated -gt $datecuto
 
 Write-Host "===== Check Password Last set of Administrator =====" -ForegroundColor Black -BackgroundColor Yellow
 Get-ADUser -Identity administrator -Properties passwordlastset | Select-Object DistinguishedName, PasswordLastSet
+Start-Sleep -Seconds 8
 
 Write-Host "===== Check Scanmail Services =====" -ForegroundColor Black -BackgroundColor Yellow
 Get-Service -Name ScanMail_Master | Select-Object Name, Status, StartType 
@@ -49,6 +70,7 @@ Write-Host "===== Check Autostart Folder =====" -ForegroundColor Black -Backgrou
 Get-ChildItem "$ENV:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 Get-ChildItem  "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 
-Write-Host "===== Check Drafts Folder of your Administrator User =====" -ForegroundColor Black -BackgroundColor Yellow
+Write-Host "===== Check Drafts Folder of your Administrator User =====" -ForegroundColor Black -BackgroundColor Yellow´Write-Host "Check in Browser"
 Start-Process "https://localhost/owa"
 
+Remove-PSSession $session
